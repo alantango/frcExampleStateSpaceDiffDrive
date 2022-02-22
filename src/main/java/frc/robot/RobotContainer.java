dfcf7e5715ca48efbd7b072371ddf582bd6d7b96
Alan Tang <alantango@gmail.com>
2022-02-21 21:01:06 -0500

    add initWait() for auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,6 +34,7 @@ import java.util.List;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
@@ -46,13 +47,17 @@ public class RobotContainer {
 
     // set up possible auto start position
     m_chooser.setDefaultOption("None", null);
-    m_chooser.addOption("T1-Center","T1-Center"); //
-    m_chooser.addOption("T2-Center","T2-Center");
     m_chooser.addOption("T1-Left","T1-Left");
+    m_chooser.addOption("T1-Center","T1-Center");
     m_chooser.addOption("T1-Right","T1-Right");
     m_chooser.addOption("T2-Left", "T2-Left");
+    m_chooser.addOption("T2-Center","T2-Center");
     m_chooser.addOption("T2-Right", "T2-Right");
     SmartDashboard.putData(m_chooser);
+
+    //init delay of auto run
+    SmartDashboard.putNumber("Auto Init Delay: ", 1);
+    SmartDashboard.putNumber("Auto Command Pause: ", 0.5);
 
     // Configure the button bindings
     configureButtonBindings();
@@ -117,7 +122,7 @@ public class RobotContainer {
     switch(position){
       case "T1-Left":
         ax=6.5; ay=5.5; ah=-44;
-        cmd = pause().andThen(
+        cmd = initWait().andThen(
           maneuver(-0.5, 0, 0.65)).andThen(
           maneuver(0, 0.23, 0.5)).andThen(
           maneuver(0.4, -0.20, 2.68)).andThen(
@@ -125,14 +130,14 @@ public class RobotContainer {
         break;
       case "T1-Center":
         ax=6.1; ay=4.95; ah=-21;
-        cmd = pause().andThen(
+        cmd = initWait().andThen(
           maneuver(-0.5, 0, 0.75)).andThen(
           maneuver(0.5, 0, 1.38)).andThen(
             fullStop());
         break;
       case "T1-Right":
         ax=6; ay=4.13; ah=0;
-        cmd = pause().andThen(
+        cmd = initWait().andThen(
           maneuver(-0.5, 0, 0.7)).andThen(
           maneuver(0, -0.23, 0.5)).andThen(
           maneuver(0.4, 0.20, 2.71)).andThen(
@@ -140,7 +145,7 @@ public class RobotContainer {
           break;
       case "T2-Left":
         ax=6.85; ay=2.35; ah=44;
-        cmd = pause().andThen(
+        cmd = initWait().andThen(
           maneuver(-0.5, 0, 0.65)).andThen(
           maneuver(0, 0.23, 0.5)).andThen(
           maneuver(0.4, -0.20, 2.68)).andThen(
@@ -148,14 +153,14 @@ public class RobotContainer {
           break;
       case "T2-Center":
         ax=7.45; ay=1.97; ah=70;
-        cmd = pause().andThen(
+        cmd = initWait().andThen(
           maneuver(-0.5, 0, 0.75)).andThen(
           maneuver(0.5, 0, 1.4)).andThen(
             fullStop());
         break;
       case "T2-Right":
         ax=8.2; ay=1.85; ah=90;
-        cmd = pause().andThen(
+        cmd = initWait().andThen(
           maneuver(-0.5, 0, 0.7)).andThen(
           maneuver(0, -0.23, 0.5)).andThen(
           maneuver(0.4, 0.20, 2.71)).andThen(
@@ -185,11 +190,17 @@ public class RobotContainer {
     return new WaitCommand(seconds);
   }
 
-  private Command pause(){
-    return pause(1);
+  private Command initWait(){
+    double s = SmartDashboard.getNumber("Auto Init Delay: ", 1);
+    return pause(s);
   }
   
-
+  private Command pause(){
+    double s = SmartDashboard.getNumber("Auto Command Pause: ", 1);
+    return pause(s);
+  }
+  
+/* 
   public Command getRamesetAutoCommand() {
     // Create a voltage constraint to ensure we don't accelerate too fast
     var autoVoltageConstraint =
@@ -248,4 +259,5 @@ public class RobotContainer {
     // Run path following command, then stop at the end.
     return ramseteCommand.andThen(() -> m_robotDrive.tankDriveVolts(0, 0));
   }
+ */
 }
